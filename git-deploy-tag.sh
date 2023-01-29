a0f275e03f1fafcb29bf31b3a623f9c9afdee3cc
annelguplus <annelguplus@gmail.com>
2023-01-30 08:43:15 +0900

    shell script 주석 추가

--- git-deploy-tag.sh
+++ git-deploy-tag.sh
@@ -1,10 +1,8 @@
-# 
+# 현재 tag 정보 조회
 TAG_BRANCH=`git describe --tags --abbrev=0`
 git checkout $TAG_BRANCH
 
-#
+# 운영 배포를 위한 tag 생성
 NEW_TAG=$(echo $TAG_BRANCH | cut -d '_' -f1)
 git tag $NEW_TAG -m "Release $NEW_TAG"
 git push origin $NEW_TAG
-
-#
